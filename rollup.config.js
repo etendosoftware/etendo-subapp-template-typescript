5de6bae5c95c09d3cb8e913a547ddd275e584dae
Leandro Allemandi <leandro.allemandi@smfconsulting.es>
2023-10-25 16:10:45 -0300

    Feature EPL-1046: change path output bundle

--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,6 @@
 import typescript from '@rollup/plugin-typescript';
 import peerDepsExternal from 'rollup-plugin-peer-deps-external';
 import json from '@rollup/plugin-json';
-import image from '@rollup/plugin-image';
 
 const pkg = JSON.parse(
   require('fs').readFileSync(
@@ -16,7 +15,7 @@ export default {
   input: './App.tsx',
   output: [
     {
-      file: '../web/com.etendorx.subapp.base/subappexample.js',
+      file: '../web/com.etendoerp.subapp.base/subappexample.js',
       format: 'cjs',
       exports: 'auto',
       strict: false,
@@ -28,11 +27,6 @@ export default {
     peerDepsExternal(),
     json({compact: true}),
     typescript(),
-    image({
-      extensions: /\.(png|jpg|jpeg|gif|svg)$/,
-      limit: 10000,
-      output: '../web/com.etendorx.subapp.base/images',
-    }),
   ],
   external,
 };
