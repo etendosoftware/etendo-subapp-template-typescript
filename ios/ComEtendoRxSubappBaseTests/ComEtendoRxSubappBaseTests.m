6007f5d3b08ceaccea7a93d8ffbb350288367f4f
Leandro <leandro.allemandi@smfconsulting.es>
2024-05-15 10:07:35 -0300

    Feature EE-945: rename test in ios

--- ios/ComEtendoRxSubappBaseTests/ComEtendoRxSubappBaseTests.m
+++ ios/ComEtendoRxSubappBaseTests/ComEtendoRxSubappBaseTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface CameraEtendoTests : XCTestCase
+@interface ComEtendoRxSubappBaseTests : XCTestCase
 
 @end
 
-@implementation CameraEtendoTests
+@implementation ComEtendoRxSubappBaseTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL (^)(UIView *view))test
 {
